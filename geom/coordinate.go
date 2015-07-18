f996e42b41203f9811340966f56a4049d724f25e
Lev Dragunov <lgardster@gmail.com>
2015-07-18 21:18:04 +0300

    Structure names refactoring. Allow access to fields.

--- geom/coordinate.go
+++ geom/coordinate.go
@@ -1,30 +1,34 @@
 package geom
 
 type Coordinate struct {
-	lat float32
-	lon float32
+	Lat float32
+	Lon float32
 }
 
 type BBox struct {
-	min Coordinate
-	max Coordinate
+	Min Coordinate
+	Max Coordinate
+}
+
+func New(lat float32, lon float32) Coordinate {
+	return Coordinate{lat, lon}
 }
 
 func CalculateBBox(coords []Coordinate) BBox {
 	minCoord := coords[0]
 	maxCoord := coords[0]
 	for _, v := range coords {
-		if v.lat < minCoord.lat {
-			minCoord.lat = v.lat
+		if v.Lat < minCoord.Lat {
+			minCoord.Lat = v.Lat
 		}
-		if v.lon < minCoord.lon {
-			minCoord.lon = v.lon
+		if v.Lon < minCoord.Lon {
+			minCoord.Lon = v.Lon
 		}
-		if v.lat > maxCoord.lat {
-			maxCoord.lat = v.lat
+		if v.Lat > maxCoord.Lat {
+			maxCoord.Lat = v.Lat
 		}
-		if v.lon > maxCoord.lon {
-			maxCoord.lon = v.lon
+		if v.Lon > maxCoord.Lon {
+			maxCoord.Lon = v.Lon
 		}
 	}
 	return BBox{minCoord, maxCoord}
--- geom/coordinate_test.go
+++ geom/coordinate_test.go
@@ -7,20 +7,28 @@ import (
 
 func TestCoordinateCreation(t *testing.T) {
 	a := Coordinate{35.67, 38.25}
-	if a.lat != 35.67 {
-		t.Error("Excepted 35.67, found", a.lat)
+	if a.Lat != 35.67 {
+		t.Error("Excepted 35.67, found", a.Lat)
 	}
-	if a.lon != 38.25 {
-		t.Error("Excepted 38.25, found", a.lon)
+	if a.Lon != 38.25 {
+		t.Error("Excepted 38.25, found", a.Lon)
+	}
+}
+
+func TestCoordinateConstructor(t *testing.T) {
+	a := Coordinate{23.34, -123.45}
+	b := New(23.34, -123.45)
+	if a.Lat != b.Lat || a.Lon != b.Lon {
+		t.Error("Constructors are not equal", a, b)
 	}
 }
 
 func TestCalculateBBox(t *testing.T) {
 	bbox := CalculateBBox([]Coordinate{{1, 2}, {3, 4}, {5, 6}})
-	if !reflect.DeepEqual(bbox.min, Coordinate{1, 2}) {
-		t.Error("Excepted {1,2}, found", bbox.min)
+	if !reflect.DeepEqual(bbox.Min, Coordinate{1, 2}) {
+		t.Error("Excepted {1,2}, found", bbox.Min)
 	}
-	if !reflect.DeepEqual(bbox.max, Coordinate{5, 6}) {
-		t.Error("Excepted {5,6}, found", bbox.max)
+	if !reflect.DeepEqual(bbox.Max, Coordinate{5, 6}) {
+		t.Error("Excepted {5,6}, found", bbox.Max)
 	}
 }
